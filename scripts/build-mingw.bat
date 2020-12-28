e41dbbb315ccdb0e62e428c3c098d753f01f428e
Denis Glazkov <glazzk.off@mail.ru>
2020-12-28 03:59:44 +0300

    Migrate libs compilation from cmake to .bat .sh scripts

---
+++ scripts/build-mingw.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo Build x86 shared library ...
+gcc86 -shared -m32 -DBUILD_SHARED ..\src\*.c -o ..\builds\libs\shared\mingw\libcliac.dll -Wl,-out-implib,..\builds\libs\shared\mingw\libcliac.a
+
+echo Build x64 shared library ...
+gcc64 -shared -m64 -DBUILD_SHARED ..\src\*.c -o ..\builds\libs\shared\mingw\x64\libcliac.dll -Wl,-out-implib,..\builds\libs\shared\mingw\x64\libcliac.a
+
+echo Build x86 static library ...
+cd ..\builds\libs\static\mingw\
+gcc86 -m32 -DBUILD_STATIC -c ..\..\..\..\src\*.c
+ar rvs libcliac.a *.o
+del "*.o" /s /q
+
+echo Build x64 static library ...
+cd x64
+gcc64 -m64 -DBUILD_STATIC -c ..\..\..\..\..\src\*.c
+ar64 rvs libcliac.a *.o
+del "*.o" /s /q
+
+echo Copy include directory ...
+cd ..\..\..\..\..\scripts
+xcopy /e /i /y ..\include ..\builds\libs\shared\mingw\include
+xcopy /e /i /y ..\include ..\builds\libs\static\mingw\include
---
+++ scripts/build-msvc.bat
@@ -0,0 +1,43 @@
+@echo off
+
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86 %*
+
+echo Build x86 shared library ...
+cl /EHsc /c /DBUILD_SHARED ..\src\*.c /I..\include\
+link /dll /out:libcliac.dll /implib:libcliac.lib *.obj
+xcopy /e /i /y "*.dll" ..\builds\libs\shared\msvc\ 
+xcopy /e /i /y "*.lib" ..\builds\libs\shared\msvc\ 
+del "*.dll" /s /q
+del "*.lib" /s /q
+del "*.obj" /s /q
+del "*.exp" /s /q
+
+echo Build x86 static library ...
+cl /EHsc /c /DBUILD_STATIC ..\src\*.c /I..\include\
+lib *.obj /OUT:libcliac.lib
+xcopy /e /i /y "*.lib" ..\builds\libs\static\msvc\ 
+del "*.lib" /s /q
+del "*.obj" /s /q
+
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %* 
+
+echo Build x64 shared library ...
+cl /EHsc /c /DBUILD_SHARED ..\src\*.c /I..\include\
+link /dll /out:libcliac.dll /implib:libcliac.lib *.obj
+xcopy /e /i /y "*.dll" ..\builds\libs\shared\msvc\x64 
+xcopy /e /i /y "*.lib" ..\builds\libs\shared\msvc\x64 
+del "*.dll" /s /q
+del "*.lib" /s /q
+del "*.obj" /s /q
+del "*.exp" /s /q
+
+echo Build x64 static library ...
+cl /EHsc /c /DBUILD_STATIC ..\src\*.c /I..\include\
+lib *.obj /OUT:libcliac.lib
+xcopy /e /i /y "*.lib" ..\builds\libs\static\msvc\x64 
+del "*.lib" /s /q
+del "*.obj" /s /q
+
+echo Copy include directory ...
+xcopy /e /i /y ..\include ..\builds\libs\shared\msvc\include
+xcopy /e /i /y ..\include ..\builds\libs\static\msvc\include
