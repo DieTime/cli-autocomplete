76a05011482d526a0f17a8beb8f2f680f6836780
Denis Glazkov <glazzk.off@mail.ru>
2020-12-28 10:41:29 +0300

    Add bash script for building unix libs

---
+++ scripts/build-unix.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir -p ../builds/libs/shared/unix/x64/
+mkdir -p ../builds/libs/static/unix/x64/
+
+echo Build x86 shared library ...
+gcc -shared -fPIC -m32 ../src/*.c -o ../builds/libs/shared/unix/libcliac.so -Wl,-out-implib,../builds/libs/shared/unix/libcliac.a
+
+echo Build x64 shared library ...
+gcc -shared -fPIC -m64 ../src/*.c -o ../builds/libs/shared/unix/x64/libcliac.so -Wl,-out-implib,../builds/libs/shared/unix/x64/libcliac.a
+
+echo Build x86 static library ...
+cd ../builds/libs/static/unix/ || exit
+gcc -m32 -c ../../../../src/*.c
+ar rvs libcliac.a *.o
+find . -type f -name '*.o' -exec rm {} +
+
+echo Build x64 static library ...
+cd x64
+gcc -m64 -c ../../../../../src/*.c
+ar rvs libcliac.a *.o
+find . -type f -name '*.o' -exec rm {} +
+
+echo Copy include directory ...
+cd ../../../../../scripts
+cp -rf ../include-shared/. ../builds/libs/shared/unix/include
+cp -rf ../include-static ../builds/libs/static/unix/include
